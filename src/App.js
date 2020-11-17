a7255982cfdab38d263510270a473f7fa5f10318
Joshua Roman <jro@Joshuas-MacBook-Pro.local>
2020-11-17 15:10:12 -0700

    successfully searching via search bar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
+import Container from '@material-ui/core/Container';
+
 import logo from './logo.svg';
 import './App.css';
+import { Home } from './components/home'
+import { AppContainer } from './components/common/Layout'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container maxWidth="sm">
+      <Home />
+    </Container>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import CssBaseline from '@material-ui/core/CssBaseline';
+
+
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
+    {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+    <CssBaseline />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
