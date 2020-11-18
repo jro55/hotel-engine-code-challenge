8b2138f275a29cf883f0e4de94cda8f0018d8415
Joshua Roman <jro@Joshuas-MacBook-Pro.local>
2020-11-18 08:50:51 -0700

    added reach router for simple routing

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import Container from '@material-ui/core/Container';
 
 import './App.css';
 import { Home } from './components/home'
+import Router from './Router';
 
 function App() {
   console.warn("No warnings here! Oh wait, isn't this a warning?")
   return (
     <Container>
-      <Home />
+      <Router />
     </Container>
   );
 }
